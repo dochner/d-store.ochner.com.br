9d1d85ca49084f88700fb72f14164abd3d9f65b8
dochner <35746980+dochner@users.noreply.github.com>
2022-09-19 21:31:35 -0300

    initialize repository with nuxt and tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
+import { nuxtConfigBuild } from './config/nuxt/build';
+import { nuxtConfigTailwind } from './config/nuxt/tailwind';
+import { nuxtConfigHead } from './config/nuxt/head';
+import { nuxtConfigContent } from './config/nuxt/content';
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  ...nuxtConfigBuild,
+  components: true,
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: nuxtConfigHead,
+  },
+  loading: {
+    color: '#02bb4d',
+  },
+  tailwindcss: nuxtConfigTailwind,
+  content: nuxtConfigContent,
+});
